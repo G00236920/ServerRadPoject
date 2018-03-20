6e3291455d775f7973459a28b9b090b6cd39dd81
Michael Kidd <G00236920@gmit.ie>
2018-03-20 19:22:42 +0000

    Change file names to match Student ID

--- G00236920/src/test/java/com/ships/G00236920ApplicationTests.java
+++ G00236920/src/test/java/com/ships/G00236920ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GXXXXXXXXApplicationTests {
+public class G00236920ApplicationTests {
 
 	@Test
 	public void contextLoads() {
