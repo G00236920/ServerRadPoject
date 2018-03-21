e4f8c5f5253c26c13b8c320d1ed5030b11ff6777
Michael Kidd <G00236920@gmit.ie>
2018-03-21 14:26:11 +0000

    JSP added and AddShip Form added

--- G00236920/src/main/java/com/ships/controller/MainController.java
+++ G00236920/src/main/java/com/ships/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.ships.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ships.model.OrderInfo;
 import com.ships.model.Ship;
@@ -10,44 +15,43 @@ import com.ships.model.ShippingCompany;
 @Controller
 public class MainController {
 	
-	@RequestMapping(value = "/showShips")
-	public String showShips() {
-		
-		return "showShips";
-		
+	@RequestMapping(value = "/showShips", method=RequestMethod.GET)
+	public String showShips(HttpServletRequest request, Model m) {
+		m.addAttribute("/showShips", request.getRequestURI());
+		return "forward:showShips.jsp";
 	}
 	
-	@RequestMapping(value = "/showShippingCompanies")
-	public String showShippingCompanies() {
-		
-		return "showShippingCompanies";
+	@RequestMapping(value = "/showShippingCompanies", method=RequestMethod.GET)
+	public String showShippingCompanies(HttpServletRequest request, Model m) {
+		m.addAttribute("/showShippingCompanies", request.getRequestURI());
+		return "forward:showShippingCompanies.jsp";
 	}
 	
-	@RequestMapping(value = "/showOrders")
-	public String showOrders() {
-		
-		return "showOrders";
+	@RequestMapping(value = "/showOrders", method=RequestMethod.GET)
+	public String showOrders(HttpServletRequest request, Model m) {
+		m.addAttribute("/showOrders", request.getRequestURI());
+		return "forward:showOrders.jsp";
 	}
 	
-	@RequestMapping(value = "/addShip")
+	@RequestMapping(value = "/addShip", method=RequestMethod.GET)
 	public String addNewShip( 
 		@ModelAttribute("ship") Ship s) {
 		
-		return "addShip";
+		return "forward:addShip.jsp";
 	}
 	
-	@RequestMapping(value = "/addShippingCompany")
+	@RequestMapping(value = "/addShippingCompany", method=RequestMethod.GET)
 	public String addNewShippingCompany( 
 		@ModelAttribute("company") ShippingCompany c) {
 		
-		return "addShippingCompany";
+		return "forward:addShippingCompany.jsp";
 	}
 	
-	@RequestMapping(value = "/createOrder")
+	@RequestMapping(value = "/createOrder", method=RequestMethod.GET)
 	public String createNewOrder( 
 		@ModelAttribute("order") OrderInfo o) {
 		
-		return "createOrder";
+		return "forward:createOrder.jsp";
 	}
 
 }
---
+++ G00236920/src/main/webapp/addShip.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<title>Add New Ship</title>
+</head>
+<body>
+
+	<form:form modelAttribute = "ship">
+		<table>
+			<tr>
+				<td>Ship ID:</td>
+				<td><form:input path="sid"></form:input></td>
+			</tr>
+			<tr>
+				<td>Ship Name:</td>
+				<td><form:input path="name"></form:input></td>
+			</tr>
+			<tr>
+				<td>No. of Passengers:</td>
+				<td><form:input path="passengers"></form:input></td>
+			</tr>
+			<tr>
+				<td>Cost:</td>
+				<td><form:input path="cost"></form:input></td>
+			</tr>
+			<tr>
+				<td>Metres:</td>
+				<td><form:input path="metres"></form:input></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="Add"/></td>
+			</tr>
+		</table>
+	</form:form>
+
+</body>
+</html>
